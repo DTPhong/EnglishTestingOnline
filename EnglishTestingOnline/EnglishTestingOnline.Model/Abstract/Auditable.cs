9dbdb28cc6d428a3c8438afaa566ada0a83a1c6d
Phong Dinh <kassssak1008@gmail.com>
2018-10-08 14:04:40 +0700

    CodingData-Phong-10/08/2018

---
+++ EnglishTestingOnline/EnglishTestingOnline.Model/Abstract/Auditable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishTestingOnline.Model.Abstract
+{
+    public abstract class Auditable : IAuditable
+    {
+        public DateTime? CreatedDate { set; get; }
+
+        [MaxLength(256)]
+        public string CreatedBy { set; get; }
+
+        public DateTime? UpdatedDate { set; get; }
+
+        [MaxLength(256)]
+        public string UpdatedBy { set; get; }
+    }
+}
---
+++ EnglishTestingOnline/EnglishTestingOnline.Model/Abstract/IAuditable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishTestingOnline.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { set; get; }
+        string CreatedBy { set; get; }
+        DateTime? UpdatedDate { set; get; }
+        string UpdatedBy { set; get; }
+    }
+}
