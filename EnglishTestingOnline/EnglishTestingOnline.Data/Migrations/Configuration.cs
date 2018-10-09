c4a5fd739cc7788c2a1060aeba3bb8dec7bf126e
Phong Dinh <kassssak1008@gmail.com>
2018-10-09 14:48:29 +0700

    add some sample data

--- EnglishTestingOnline/EnglishTestingOnline.Data/Migrations/Configuration.cs
+++ EnglishTestingOnline/EnglishTestingOnline.Data/Migrations/Configuration.cs
@@ -1,9 +1,11 @@
 namespace EnglishTestingOnline.Data.Migrations
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using EnglishTestingOnline.Model.Model;
 
     internal sealed class Configuration : DbMigrationsConfiguration<EnglishTestingOnline.Data.EnglishDbContext>
     {
@@ -14,10 +16,59 @@ namespace EnglishTestingOnline.Data.Migrations
 
         protected override void Seed(EnglishTestingOnline.Data.EnglishDbContext context)
         {
+            CreateChudeSample(context);
+            CreateLoaiCauHoiSample(context);
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+        }
+
+        private void CreateChudeSample(EnglishDbContext context)
+        {
+            if (context.ChuDes.Count() == 0)
+            {
+                List<ChuDe> listChuDe = new List<ChuDe>()
+            {
+                    new ChuDe()
+                    {
+                        TenChuDe="Reading"
+                    },
+                    new ChuDe()
+                    {
+                        TenChuDe="Speaking"
+                    },
+                    new ChuDe()
+                    {
+                        TenChuDe="Listening"
+                    },
+                    new ChuDe()
+                    {
+                        TenChuDe="Writing"
+                    }
+            };
+                context.ChuDes.AddRange(listChuDe);
+                context.SaveChanges();
+            }
+        }
+        private void CreateLoaiCauHoiSample(EnglishDbContext context)
+        {
+            if (context.LoaiCauHois.Count() == 0)
+            {
+                List<LoaiCauHoi> listLoaiCauHoi = new List<LoaiCauHoi>()
+            {
+                    new LoaiCauHoi()
+                    {
+                        NoiDung="Multiple-choice"
+                    },
+                    new LoaiCauHoi()
+                    {
+                        NoiDung="Essay"
+                    },
+            };
+                context.LoaiCauHois.AddRange(listLoaiCauHoi);
+                context.SaveChanges();
+            }
         }
     }
 }
