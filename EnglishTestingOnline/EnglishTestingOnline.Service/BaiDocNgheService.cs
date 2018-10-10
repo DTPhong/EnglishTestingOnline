69111fd4ab66aea9fe9781eec17748b22ba2196c
Phong Dinh <kassssak1008@gmail.com>
2018-10-10 13:37:05 +0700

    set up service + unit test service

---
+++ EnglishTestingOnline/EnglishTestingOnline.Service/BaiDocNgheService.cs
@@ -0,0 +1,62 @@
+﻿using EnglishTestingOnline.Data.Infrastructure;
+using EnglishTestingOnline.Data.Respositories;
+using EnglishTestingOnline.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishTestingOnline.Service
+{
+    public interface IBaiDocNgheSercive
+    {
+        BaiDocNghe Add(BaiDocNghe baiDocNghe);
+        BaiDocNghe Delete(int id);
+        void Update(BaiDocNghe baiDocNghe);
+        BaiDocNghe GetById(int id);
+        IEnumerable<BaiDocNghe> GetAll();
+        void Save();
+    }
+    public class BaiDocNgheService : IBaiDocNgheSercive
+    {
+        IBaiDocNgheRepository _baiDocNgheRepository;
+        IUnitOfWork _unitOfWork;
+
+        public BaiDocNgheService(IBaiDocNgheRepository baiDocNgheRepository, IUnitOfWork unitOfWork)
+        {
+            this._baiDocNgheRepository = baiDocNgheRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public BaiDocNghe Add(BaiDocNghe baiDocNghe)
+        {
+            return _baiDocNgheRepository.Add(baiDocNghe);
+        }
+
+        public BaiDocNghe Delete(int id)
+        {
+            return _baiDocNgheRepository.Delete(id);
+        }
+
+        public IEnumerable<BaiDocNghe> GetAll()
+        {
+            return _baiDocNgheRepository.GetAll();
+        }
+
+        public BaiDocNghe GetById(int id)
+        {
+            return _baiDocNgheRepository.GetSingleById(id);
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(BaiDocNghe baiDocNghe)
+        {
+            _baiDocNgheRepository.Update(baiDocNghe);
+        }
+    }
+}
---
+++ EnglishTestingOnline/EnglishTestingOnline.UnitTest/ServiceTest/CauHoiServiceTest.cs
@@ -0,0 +1,87 @@
+﻿using EnglishTestingOnline.Data.Infrastructure;
+using EnglishTestingOnline.Data.Respositories;
+using EnglishTestingOnline.Model.Model;
+using EnglishTestingOnline.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishTestingOnline.UnitTest.ServiceTest
+{
+    [TestClass]
+    public class ProductServiceTest
+    {
+        private Mock<ICauHoiRepository> _mockRepository;
+        private Mock<IUnitOfWork> _mockUnitOfWork;
+        private ICauhoiService _cauhoiService;
+        private List<CauHoi> _listCauhoi;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockRepository = new Mock<ICauHoiRepository>();
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _cauhoiService = new CauHoiService(_mockRepository.Object, _mockUnitOfWork.Object);
+            _listCauhoi = new List<CauHoi>()
+            {
+                new CauHoi() {ID=1, LoaiCauHoi_ID=1, BaiDocNghe_ID=1, ChuDe_ID=3, NoiDung="Lorem lorem lorem", DapAn="lorem" },
+                new CauHoi() {ID=2, LoaiCauHoi_ID=1, BaiDocNghe_ID=1, ChuDe_ID=3, NoiDung="Lorem lorem lorem", DapAn="lorem" },
+                new CauHoi() {ID=3, LoaiCauHoi_ID=1, BaiDocNghe_ID=1, ChuDe_ID=3, NoiDung="Lorem lorem lorem", DapAn="lorem" },
+                new CauHoi() {ID=4, LoaiCauHoi_ID=1, BaiDocNghe_ID=1, ChuDe_ID=3, NoiDung="Lorem lorem lorem", DapAn="lorem" },
+                new CauHoi() {ID=5, LoaiCauHoi_ID=1, BaiDocNghe_ID=1, ChuDe_ID=3, NoiDung="Lorem lorem lorem", DapAn="lorem" }
+            };
+        }
+
+        [TestMethod]
+        public void Product_Service_GetAll()
+        {
+            //setup method
+            _mockRepository.Setup(m => m.GetAll(null)).Returns(_listCauhoi);
+
+            //call action
+            var result = _cauhoiService.GetAll() as List<CauHoi>;
+
+            //compare
+            Assert.IsNotNull(result);
+            Assert.AreEqual(5, result.Count);
+        }
+
+        //[TestMethod]
+        //public void Product_Service_GetRelatedProduct()
+        //{
+        //    //setup method
+        //    _mockRepository.Setup(m => m.GetAll(null)).Returns(_listProduct);
+
+        //    //call action
+        //    var result = _productService.GetReatedProducts(22,4) as List<Product>;
+
+        //    //compare
+        //    Assert.IsNotNull(result);
+        //    Assert.AreEqual(4, result.Count);
+        //}
+
+        //[TestMethod]
+        //public void Product_Service_Create()
+        //{
+        //    Product product = new Product();
+        //    product.Name = "Test";
+        //    product.Alias = "test";
+        //    product.Status = true;
+
+        //    _mockRepository.Setup(m => m.Add(product)).Returns((Product p) =>
+        //    {
+        //        p.ID = 1;
+        //        return p;
+        //    });
+
+        //    var result = _productService.Add(product);
+
+        //    Assert.IsNotNull(result);
+        //    Assert.AreEqual(1, result.ID);
+        //}
+    }
+}
