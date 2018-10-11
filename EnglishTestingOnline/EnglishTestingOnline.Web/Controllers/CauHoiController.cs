b90746b882beb6bddf362611e4d4c0df9b2d17bc
Phong Dinh <kassssak1008@gmail.com>
2018-10-11 09:30:33 +0700

    cay vl

--- EnglishTestingOnline/EnglishTestingOnline.Web/Controllers/CauHoiController.cs
+++ EnglishTestingOnline/EnglishTestingOnline.Web/Controllers/CauHoiController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Serialization;
 
 namespace EnglishTestingOnline.Web.Controllers
 {
@@ -24,5 +25,20 @@ namespace EnglishTestingOnline.Web.Controllers
             var viewModel = Mapper.Map<IEnumerable<CauHoi>, IEnumerable<CauHoiViewModel>>(model);
             return View(viewModel);
         }
+
+        //public void write()
+        //{
+
+
+        //    System.Xml.Serialization.XmlSerializer writer =
+        //        new System.Xml.Serialization.XmlSerializer(viewModel.GetType());
+
+        //    var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//SerializationOverview.xml";
+        //    System.IO.FileStream file = System.IO.File.Create(path);
+
+        //    writer.Serialize(file, viewModel);
+        //    file.Close();
+        //}
+
     }
 }
