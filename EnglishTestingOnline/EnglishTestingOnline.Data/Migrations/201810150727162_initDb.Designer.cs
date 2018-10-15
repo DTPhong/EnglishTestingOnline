bb466cd57c2060cf844fde11b1c4ec8f1a0c5abf
Thanh Phong <phongdt1008@gmail.com>
2018-10-15 16:53:33 +0700

    initDb

--- EnglishTestingOnline/EnglishTestingOnline.Data/Migrations/201810150727162_initDb.Designer.cs
+++ EnglishTestingOnline/EnglishTestingOnline.Data/Migrations/201810150727162_initDb.Designer.cs
@@ -7,13 +7,13 @@ namespace EnglishTestingOnline.Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class addshocvien : IMigrationMetadata
+    public sealed partial class initDb : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(addshocvien));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initDb));
         
         string IMigrationMetadata.Id
         {
-            get { return "201810090829118_add-s-hocvien"; }
+            get { return "201810150727162_initDb"; }
         }
         
         string IMigrationMetadata.Source
