c822c240aa2055638f8626c0b84d5b14cd42b5a1
DESKTOP-M3Q5KNJ\MCT <chithuan102@gmail.com>
2018-11-04 22:16:03 +0700

    complete view part 1

---
+++ EnglishTestingOnline/EnglishTestingOnline.Model/Model/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EnglishTestingOnline.Model.Model
+{
+    [Table("Tests")]
+    public class Test : IdentityUser
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        [StringLength(250)]
+        public string Name { get; set; }
+    }
+}
